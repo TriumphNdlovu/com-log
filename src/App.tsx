5d08e3043108ac86f4c977355d216e0146f443d8
TriumphNdlovu <realtriumphndlovu@gmail.com>
2025-10-08 09:26:23 +0200

    Build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Post from './pages/Post'
-import About from './pages/About'
-import Animal from './pages/Animal'
+
 
 export default function App() {
   return (
@@ -10,14 +7,5 @@ export default function App() {
     <>
       <Home />
     </>
-
-
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/post/:slug" element={<Post />} />
-    //     <Route path="/about" element={<About />} />
-    //   </Routes>
-    // </BrowserRouter>
   )
 }
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { ArrowLeft } from 'lucide-react'
 import ReactMarkdown from 'react-markdown'
 import frontMatter from 'front-matter'
 
